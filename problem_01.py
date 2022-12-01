5f9520f7219efcdc67756b7ee4a64783ab8ff2aa
Nakul Khanna <nakul@charmtx.com>
2022-12-01 11:49:15 +0000

    Formatting

--- problem_01.py
+++ problem_01.py
@@ -2,12 +2,10 @@ from pathlib import Path
 
 input_text = Path("problem_01.txt").read_text().strip()
 
+
 def get_elf_nums():
     elf_texts = input_text.split("\n\n")
-    return (
-        sum(int(x) for x in elf_text.split("\n"))
-        for elf_text in elf_texts
-    )
+    return (sum(int(x) for x in elf_text.split("\n")) for elf_text in elf_texts)
 
 
 def part_a():
@@ -16,8 +14,10 @@ def part_a():
 
 print(part_a())
 
+
 def part_b():
     sorted_elf_nums = sorted(list(get_elf_nums()))
     return sum(sorted_elf_nums[-3:])
+
 
 print(part_b())

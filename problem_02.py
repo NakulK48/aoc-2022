c5ac151d6c89d168e9aa017a0ad9462a0d651b36
Nakul Khanna <nakul@charmtx.com>
2022-12-02 09:00:20 +0000

    Problem 2 without hardcoding

--- problem_02.py
+++ problem_02.py
@@ -1,51 +1,60 @@
 from pathlib import Path
+from collections.abc import Generator
 
 input_text = Path("problem_02.txt").read_text().strip()
 
 
-def input_pairs():
+class Move:
+    def __init__(self, value: int):
+        self.value = value
+
+    @staticmethod
+    def from_opp(letter: str) -> "Move":
+        return Move(ord(letter) - ord("A"))
+
+    @staticmethod
+    def from_me(letter: str) -> "Move":
+        return Move(ord(letter) - ord("X"))
+
+    def own_score(self) -> int:
+        return self.value + 1
+
+    def score(self, other: "Move") -> int:
+        # 1 higher means you win; 1 lower means you lose
+        diff = (self.value - other.value) % 3
+        return {0: 3, 1: 6, 2: 0}[diff]
+
+    def get_move(self, strategy: str) -> "Move":
+        new_moves = {"X": self.value - 1, "Y": self.value, "Z": self.value + 1}
+        return Move(new_moves[strategy] % 3)
+
+
+def input_pairs() -> Generator[tuple[str, str], None, None]:
     for line in input_text.split("\n"):
         yield line.split(" ")
 
 
-SCORES = {"X": 1, "Y": 2, "Z": 3}
-WIN_PAIRS = {("A", "Y"), ("B", "Z"), ("C", "X")}
-DRAW_PAIRS = {("A", "X"), ("B", "Y"), ("C", "Z")}
-LOSE_PAIRS = {("A", "Z"), ("B", "X"), ("C", "Y")}
-
-SCORE_BY_PAIR = {
-    **{win_pair: 6 for win_pair in WIN_PAIRS},
-    **{draw_pair: 3 for draw_pair in DRAW_PAIRS},
-    **{lose_pair: 0 for lose_pair in LOSE_PAIRS},
-}
+def input_moves() -> Generator[tuple[str, str], None, None]:
+    for (raw_opp, raw_my) in input_pairs():
+        yield Move.from_opp(raw_opp), Move.from_me(raw_my)
 
 
 def part_a():
     score = 0
-    for opp_move, my_move in input_pairs():
-        score += SCORES[my_move] + SCORE_BY_PAIR[(opp_move, my_move)]
+    for opp_move, my_move in input_moves():
+        score += my_move.own_score() + my_move.score(opp_move)
     return score
 
 
 print(part_a())
 
 
-SCORES_B = {"X": 0, "Y": 3, "Z": 6}
-ROCK_PAIRS = {("A", "Y"), ("B", "X"), ("C", "Z")}
-PAPER_PAIRS = {("A", "Z"), ("B", "Y"), ("C", "X")}
-SCISSORS_PAIRS = {("A", "X"), ("B", "Z"), ("C", "Y")}
-
-SCORE_BY_PAIR_B = {
-    **{pair: 1 for pair in ROCK_PAIRS},
-    **{pair: 2 for pair in PAPER_PAIRS},
-    **{pair: 3 for pair in SCISSORS_PAIRS},
-}
-
-
 def part_b():
     score = 0
-    for opp_move, my_move in input_pairs():
-        score += SCORES_B[my_move] + SCORE_BY_PAIR_B[(opp_move, my_move)]
+    for opp_move_raw, strategy in input_pairs():
+        opp_move = Move.from_opp(opp_move_raw)
+        my_move = opp_move.get_move(strategy)
+        score += my_move.own_score() + my_move.score(opp_move)
     return score
 
 
